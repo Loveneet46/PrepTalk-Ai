af9a5a4cf026a8cb099ba7e3277027e2c1d9e394
Loveneet Singh <loveneetsingh.in@gmail.com>
2025-05-05 17:31:28 +0530

    show generated interview on the homepagr

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth_action";
 
-const Page = () => {
+const Page = async () => {
+    const user = await getCurrentUser();
+
+    if (!user) {
+        return (
+            <div className="text-center mt-10">
+                <h2>Please sign in to generate an interview.</h2>
+            </div>
+        );
+    }
+
     return (
         <>
-           <h3>Interview Generation</h3>
-            <Agent userName="You" userId="user1"type="generate" />
+            <h3>Interview Generation</h3>
+
+            <Agent
+                userName={user.name || "Anonymous"}
+                userId={user.id}
+                // profileImage={user.profileURL || "/default-profile.png"}
+                type="generate"
+            />
         </>
     );
 };
